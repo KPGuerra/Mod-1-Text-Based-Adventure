b6019a084af4cfdd438273f3ec03d24c074a062e
KPGuerra <KPGuerra96@gmail.com>
2020-10-20 17:19:13 -0400

    Added a character_stats method

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,12 @@ class Character < ActiveRecord::Base
     has_many :items, through: :encounters
     has_many :enemies, through: :encounters
     belongs_to :user
+
+    ## Make a method for character stats; Role, Atk, HP etc ?
+    def self.character_stats
+        puts @character.name
+        puts @character.role
+        puts @character.attack_power
+        puts @character.hp
+    end
 end 
