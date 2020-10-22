004ed776d21e5d347342e8d53adeac3d05d9e353
KPGuerra <KPGuerra96@gmail.com>
2020-10-22 11:03:45 -0400

    Added in method to attack character

--- app/models/enemy.rb
+++ app/models/enemy.rb
@@ -2,12 +2,13 @@ class Enemy < ActiveRecord::Base
     belongs_to :encounter
     @@prompt = TTY::Prompt.new
 
-    # ENEMY MOVES ---------------------------------------------
+    
+#ENEMY MOVES ==========================================================================================
 
-    # Give enemy array of moves. Randomize through them to get attack choices. Maybe the same for character ? Or maybe scripted moves
+# Give enemy array of moves. Randomize through them to get attack choices. Maybe the same for character ? Or maybe scripted moves
     @@enemy_attacks = ['Burst', 'Ignite', 'Grip', 'Punch', 'Backstab', 'Scratch']
 
-    # Give bosses different attacks ?
+# Give bosses different attacks ?
     @@boss_attacks = ['Paralyze', 'Riptide', 'Haunt', 'Bloodlust', 'Tremblor', 'Poison']
 
     def enemy_moves
@@ -19,16 +20,15 @@ class Enemy < ActiveRecord::Base
         moves = @@boss_attacks.sample(4)
         moves
     end
+#========================================================================================================
 
-    # Will be moved to respective classes (Character, Enemy)
-    #------------------------------------------#
-    def self.attack(character) #place holder method name
-        damage = character.attack_power
+#Attacking character ====================================================================================
+    def attk_char(character)
+        damage = self.attack_power
         if damage > 0
-            self.update(hp: self.hp - damage)
-            puts "You dealt #{dmg} damage!"
-            puts "#{self.name} has #{self.hp} HP left!"
+            character.update(hp: character.hp - damage)
+            puts "You dealt #{damage} damage!"
+            puts "#{character.name} has #{character.hp} HP left!"
         end
     end
-    #------------------------------------------#
-end 
+#========================================================================================================
